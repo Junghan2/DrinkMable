232395f2ca20b66d710abcea6a2ad026d05c6f9c
이근재 <hanzen927@gmail.com>
2016-10-10 13:45:41 +0900

    서블릿 추가

--- src/main/java/bamcoding/Random.java
+++ src/main/java/bamcoding/Random.java
@@ -2,6 +2,12 @@ package bamcoding;
 
 import java.util.Scanner;
 
+/**
+ * 랜덤 함수 정의 중...
+ * 
+ * @author 206-017
+ *
+ */
 public class Random {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -9,14 +15,17 @@ public class Random {
 		String key = "";
 
 		while (true) {
-
+			// z를 입력하면 종료
 			if (key.equalsIgnoreCase("z")) {
 				break;
 			} else {
 				result = (int) (Math.random() * 6) + 1;
 				System.out.println(result);
+
 			}
+			// 아무 값이나 입력하세요.
 			key = input.nextLine();
+
 		}
 
 	}
---
+++ src/main/java/bamcoding/web/ViewCubeServlet.java
@@ -0,0 +1,36 @@
+package bamcoding.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 큐브.jsp model2 방식으로 코딩합니다.
+ * @author 206-017
+ *
+ */
+public class ViewCubeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public ViewCubeServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String viewPath = "/WEB-INF/view/bamcoding/cube.jsp";
+		RequestDispatcher rd = request.getRequestDispatcher(viewPath);
+		rd.forward(request, response);
+
+	}
+
+}
